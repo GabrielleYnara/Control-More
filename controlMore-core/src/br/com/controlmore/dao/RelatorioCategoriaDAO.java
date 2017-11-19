d83d03c6d44ed492afc1f971ff3f8effca262074
GabrielleYnara <gabrielleynara@ymail.com>
2017-11-19 17:11:02 -0200

    Consulta no banco para o relatório de comparação entre categorias.

--- controlMore-core/src/br/com/controlmore/dao/RelatorioCategoriaDAO.java
+++ controlMore-core/src/br/com/controlmore/dao/RelatorioCategoriaDAO.java
@@ -35,21 +35,33 @@ public class RelatorioCategoriaDAO extends AbstractDAO{
 	public List<EntidadeDominio> consultar(EntidadeDominio entidade) {
 		RelatorioCategoria relatorio = (RelatorioCategoria) entidade;
 		StringBuilder sql = new StringBuilder();
-
-		if(relatorio.getInicio()!=null && relatorio.getFim()!=null){
-			sql.append("SELECT SUM(valor) as valorTotal, ");
-			sql.append("c.DESCRICAO ");
-			sql.append("FROM Saida ");
-			sql.append("JOIN Categoria c ON Saida.CATEGORIA = c.id ");
-			sql.append("WHERE DataSaida BETWEEN ? AND ? ");
+		sql.append("SELECT SUM(valor) as valorTotal, ");
+		sql.append("c.DESCRICAO ");
+		if(relatorio.getCategoria1()!=null && relatorio.getCategoria2()!=null){
+		    sql.append(",TO_NUMBER(TO_CHAR(s.DataSaida, 'mm')) as mes, ");
+		    sql.append("TO_NUMBER(TO_CHAR(s.DataSaida, 'yyyy')) as ano ");
+		}
+		sql.append("FROM Saida s ");
+		sql.append("JOIN Categoria c ON s.CATEGORIA = c.id ");
+		sql.append("WHERE s.DataSaida BETWEEN ? AND ? ");
+		if(relatorio.getCategoria1()!=null && relatorio.getCategoria2()!=null){
+			sql.append("AND c.Descricao=? OR c.Descricao=? " );
+			sql.append("GROUP BY c.DESCRICAO, ");
+			sql.append("TO_NUMBER(TO_CHAR(s.DataSaida, 'mm')), ");
+		    sql.append("TO_NUMBER(TO_CHAR(s.DataSaida, 'yyyy')) ");
+		    sql.append("Order by c.DESCRICAO, ano, mes");
+		}else{
 			sql.append("GROUP BY c.DESCRICAO ");
 			sql.append("Order by c.DESCRICAO");
 		}
-		
 		try(PreparedStatement preparador = conexao.prepareStatement(sql.toString())){
 			preparador.setDate(1, new java.sql.Date(relatorio.getInicio().getTime()));
 			preparador.setDate(2, new java.sql.Date(relatorio.getFim().getTime()));
-			
+			if(relatorio.getCategoria1()!=null && relatorio.getCategoria2()!=null){
+				preparador.setString(3, relatorio.getCategoria1());
+				preparador.setString(4, relatorio.getCategoria2());
+			}
+
 			ResultSet result = preparador.executeQuery(); //passa o resultado da execução da Query para a variável result
 			List<EntidadeDominio> categorias = new ArrayList<EntidadeDominio>(); //cria uma lista de saidas
 			
@@ -57,7 +69,9 @@ public class RelatorioCategoriaDAO extends AbstractDAO{
 				RelatorioCategoria categoria = new RelatorioCategoria();
 				categoria.setDescricao(result.getString("Descricao"));
 				categoria.setValorTotal(result.getFloat("ValorTotal"));
-				
+				if(relatorio.getCategoria1()!=null && relatorio.getCategoria2()!=null){
+					categoria.setDataReferencia(result.getString("mes") + "/" + result.getString("ano"));
+				}
 				categorias.add(categoria);
 			}
 			return categorias;
--- controlMore-dominio/src/br/com/controlmore/dominio/RelatorioCategoria.java
+++ controlMore-dominio/src/br/com/controlmore/dominio/RelatorioCategoria.java
@@ -3,6 +3,10 @@ package br.com.controlmore.dominio;
 public class RelatorioCategoria extends Relatorios{
 	private float valorTotal;
 	private String descricao;
+	private String categoria1;
+	private String categoria2;
+	private String dataReferencia;
+	
 	public float getValorTotal() {
 		return valorTotal;
 	}
@@ -14,6 +18,24 @@ public class RelatorioCategoria extends Relatorios{
 	}
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
+	}
+	public String getCategoria1() {
+		return categoria1;
+	}
+	public void setCategoria1(String categoria1) {
+		this.categoria1 = categoria1;
+	}
+	public String getCategoria2() {
+		return categoria2;
+	}
+	public void setCategoria2(String categoria2) {
+		this.categoria2 = categoria2;
+	}
+	public String getDataReferencia() {
+		return dataReferencia;
+	}
+	public void setDataReferencia(String dataReferencia) {
+		this.dataReferencia = dataReferencia;
 	}
 	
 }
